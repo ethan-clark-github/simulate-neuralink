2c943bf37e9ac40caac85dcf75e7f1bbbd6d9c38
ethan clark <esclark99@gmail.com>
2023-02-20 12:11:38 -0500

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
+config.hosts << "simulate-neuralink.herokuapp.com"
+port = ENV["PORT"] || 3000
 require "active_support/core_ext/integer/time"
-
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
