297c7402fa77db7fd9052379722f99e78e9acf8d
ethan clark <esclark99@gmail.com>
2023-02-21 09:24:36 -0500

    testing

--- app/serializers/function_with_inputs_serializer.rb
+++ app/serializers/function_with_inputs_serializer.rb
@@ -1,5 +1,5 @@
-# class FunctionWithInputsSerializer < ActiveModel::Serializer
-#   attributes :id, :name, :category, :input_id, :output_id
-#   has_one :input
-#   has_one :output
-# end
+class FunctionWithInputsSerializer < ActiveModel::Serializer
+  attributes :id, :name, :category, :input_id, :output_id
+  has_one :input
+  has_one :output
+end
